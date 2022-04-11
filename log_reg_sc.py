13ad76d94a4626677f8983419885e33a279bd0b8
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-04-11 12:47:59 +0500

    Create log_reg_sc.py

---
+++ log_reg_sc.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.001, n_iters=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # gradient descent
+        for step in range(self.n_iters):
+
+            linear_model = np.dot(X, self.weights) + self.bias
+            # apply sigmoid function
+            y_predicted = self._sigmoid(linear_model)
+
+            # compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+            # update parameters
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self._sigmoid(linear_model)
+        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+        return np.array(y_predicted_cls)
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+
+# Testing
+if __name__ == "__main__":
+    
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
+
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+    bc = datasets.load_breast_cancer()
+    X, y = bc.data, bc.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0
+    )
+
+    regressor = LogisticRegression(learning_rate=0.0001, n_iters=1000)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+
+    print("Accuracy:", accuracy(y_test, predictions).round(3)*100, '%')
